59ce7793c8c63d74c645235adb3fa8977c8f0f13
Yuxuan Xu <852563495@qq.com>
2018-08-06 22:06:50 +0800

    1001

---
+++ 1001.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+  int n;
+  int m=0;
+  scanf("%d",&n);	
+  while(n>1)
+  {
+    if(n%2==0)  n=n/2;
+else  n=(3*n+1)/2;
+    m=m+1;
+  }
+  printf("%d",m);
+return 0;
+}
